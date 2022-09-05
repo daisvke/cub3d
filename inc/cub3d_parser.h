4668f280d27b25b6248a2aa86e5c8eca1bcac8a1
dtanigaw <dtanigaw@student.42.fr>
2022-09-05 23:09:41 +0200

    norm edit

--- inc/cub3d_parser.h
+++ inc/cub3d_parser.h
@@ -6,7 +6,7 @@
 /*   By: lchan <lchan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 17:10:51 by lchan             #+#    #+#             */
-/*   Updated: 2022/09/05 19:51:16 by lchan            ###   ########.fr       */
+/*   Updated: 2022/09/05 23:09:25 by mint             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,34 +57,29 @@
 
 enum	e_file_err
 {
-	/********************	ERR_FILE*/
-	ERR_NO_FILE,			//bit 0/32
-	ERR_EXTENTION,			//bit 1/32
-	ERR_FOLDER,				//bit 2/32
-	ERR_CHMOD,				//bit 3/32
-	ERR_PATH,				//bit 4/32
+	ERR_NO_FILE,
+	ERR_EXTENTION,
+	ERR_FOLDER,	
+	ERR_CHMOD,
+	ERR_PATH,
 };
 
 enum	e_map_err{
 	ERR_EMPTY_FILE,
-	/********************	ERR_TEXTURE*/
-	ERR_TX_KEY_MISS,	//not sure
+	ERR_TX_KEY_MISS,
 	ERR_TX_PATH,
 	ERR_TX_PATH_LENGH,
-	/********************	ERR_FC_C*/
 	ERR_FC_OVERFLOW,
 	ERR_FC_KEY_MISS,
-	ERR_FC_FORM,					//expected color form : F/C 255.255.255
-	/********************	ERR_MAP*/
+	ERR_FC_FORM,
 	ERR_MP_MISPLACED,
 	ERR_MP_TOO_SMALL,
 	ERR_MP_NO_PLAYER,
 	ERR_MP_MULTI_PLAYER,
 	ERR_MP_CUTTED,
 	ERR_MP_BORDERS,
-	ERR_MP_TOO_BIG,					//MP has more than 1024 line
+	ERR_MP_TOO_BIG,
 	ERR_MP_UNABLE_TO_CPY,
-	/*******************	non_blocking_err*/
 	ERR_GIBBER,
 	ERR_FC_MULTIDEF,
 	ERR_TX_MULTIDEF,
@@ -98,8 +93,8 @@ enum	e_line_type{
 	TYPE_F,
 	TYPE_C,
 	TYPE_MAP,
-	TYPE_ERR,		//line start by unvalide chars
-	TYPE_USELESS,	//line composed of space and/or \n
+	TYPE_ERR,
+	TYPE_USELESS,
 	TYPE_EOF,
 };
 
@@ -115,20 +110,20 @@ enum	e_tab_free_opt{
 
 typedef struct s_parser
 {
-	char	*line;				//current gnl line;
-	int		gnl_cnt;			//nbr of gnl calls aka line nbr at each call
+	char	*line;
+	int		gnl_cnt;
 	int		map_max_x;
 	int		map_max_y;
-	int		type;				//type of line; e_line_type
+	int		type;
 	char	info_buf[6][PATH_MAX + 2];
 	int		info_buf_line[6];
 	char	*map_buf[PARSER_BUFFER_SIZE];
-	int		map_line_buf[PARSER_BUFFER_SIZE];	//save gnl of type map
-	int		err_buf[PARSER_BUFFER_SIZE][2];	//save line numbers of time error
+	int		map_line_buf[PARSER_BUFFER_SIZE];
+	int		err_buf[PARSER_BUFFER_SIZE][2];
 	int		info_buf_flag;
-	int		map_buf_index;		//is also the size of the map
+	int		map_buf_index;
 	int		err_buf_index;
 	int		blocking_err_flag;
-}t_parser;
+}	t_parser;
 
 #endif
